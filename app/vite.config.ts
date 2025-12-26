45de328dd7cde630a6ac53d2e3b86a41534a448b
lubab <hi@lubab.dev>
2025-12-26 13:05:37 +0100

    init app

---
+++ app/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
+
+const config = defineConfig({
+	plugins: [
+		devtools(),
+		// nitro(),
+		viteTsConfigPaths({
+			projects: ['./tsconfig.json'],
+		}),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+	],
+	build: {
+		rollupOptions: {
+			external: ["./server.ts"]
+		}
+	}
+	// nitro: {
+	// 	preset: "bun",
+	// }
+})
+
+export default config
