f463a800488b4278aabfc51eb10e7de13e492514
lubab <hi@lubab.dev>
2025-12-27 08:24:23 +0100

    format utils

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -1,8 +1,8 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
 
 export function slugify(input: string): string {
@@ -11,15 +11,15 @@ export function slugify(input: string): string {
 		.normalize("NFKD")
 		.replace(/[\u0300-\u036f]/g, "")
 		.replace(/[^a-z0-9]+/g, "-")
-		.replace(/^-+|-+$/g, "")
+		.replace(/^-+|-+$/g, "");
 }
 
 export function randomStr(length = 6) {
-	return Math.random().toString(36).slice(2, 2 + length)
+	return Math.random()
+		.toString(36)
+		.slice(2, 2 + length);
 }
 
 export function truncate(str: string, maxLength: number) {
-	return str.length > maxLength
-		? str.slice(0, maxLength - 1) + "…"
-		: str;
+	return str.length > maxLength ? `${str.slice(0, maxLength - 1)}…` : str;
 }
