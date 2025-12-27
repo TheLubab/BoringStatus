b0bfa603424023af94a159a754d80fa37cfcc5a7
lubab <hi@lubab.dev>
2025-12-27 15:55:07 +0100

    fix env

--- app/drizzle.config.ts
+++ app/drizzle.config.ts
@@ -1,13 +1,11 @@
-import { config } from 'dotenv'
+import { env } from '@/env'
 import { defineConfig } from 'drizzle-kit'
-
-config()
 
 export default defineConfig({
 	out: './drizzle',
 	schema: ['./src/db/saas-schema.ts', './src/db/auth-schema.ts'],
 	dialect: 'postgresql',
 	dbCredentials: {
-		url: process.env.DATABASE_URL!,
+		url: env.DATABASE_URL,
 	},
 })
